ad740b5501a903cecafc603e0c5b4dccd6560882
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-03-12 16:23:13 +0100

    💀

---
+++ eloquent-js/000_intro-1.js
@@ -0,0 +1,26 @@
+let total = 0, count = 1;
+while (count <= 10) {
+  total += count;
+  count += 1;
+}
+
+console.log(total);
+// → 55
+
+// console.log(sum(range(1, 10)));
+// → 55
+
+function factorial(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return factorial(n - 1) * n;
+  }
+}
+
+console.log(factorial(8));
+// → 40320
+
+console.log(factorial(57))
+
+console.log("💀")
