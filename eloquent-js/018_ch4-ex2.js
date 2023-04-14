4cfdd6c6cb77685bd99dd95c7cd531434cc49553
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-04-14 22:56:10 +0200

    Did some Ch4 exercises

--- eloquent-js/018_ch4-ex2.js
+++ eloquent-js/018_ch4-ex2.js
@@ -1,2 +1,37 @@
 // Reversing an array
-// TODO
+function reverseArray(arr) {
+  let newArr = []
+  
+  for (let i = arr.length - 1; i >= 0; i--) {
+    newArr.push(arr[i]);
+  }
+
+  return newArr;
+}
+
+function reverseArrayInPlace(arr) {
+  arrLen = arr.length % 2 == 0 ? arr.length / 2 : Math.floor(arr.length / 2);
+  // Math.floor(arr.length / 2) could also be just arr.length / 2 - 0.5 because 
+  // math moment but I guess it could be also faster
+
+  for (let i = 0; i < arrLen; i++) {
+    var wasLast = arr[arr.length - i - 1];
+    var toBeLast = arr[i];
+    arr[i] = wasLast;
+    arr[arr.length - i - 1] = toBeLast;
+    // console.log(arr);
+    // let toSwap = arr[i];
+    // arr[i] = arr[arrLen - i - 1];
+    // arr[arrLen - i - 1] = toSwap;
+    // console.log(`${arr[i]} ${arr[arrLen - i - 1]}`);
+  }
+
+  return arr;
+}
+
+let testArr = [1, 2, 3, 4, 5, 6, 7];
+console.log(testArr);
+console.log(reverseArray(testArr));
+console.log(testArr);
+console.log(reverseArrayInPlace(testArr));
+console.log(testArr);
--- eloquent-js/019_ch4-ex3.js
+++ eloquent-js/019_ch4-ex3.js
@@ -1,2 +1,63 @@
 // A list
-// TODO
+function arrayToList(arr) {
+  let list = {
+    value: arr[0],
+    rest: null
+  };
+
+  for (let i = 1; i < arr.length; i++) {
+    list = {
+      value: arr[i],
+      rest: list
+    };
+  }
+  
+  return list;
+}
+
+arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arrayToList(arr));
+list = arrayToList(arr);
+console.log(list.rest.rest.rest.rest);
+
+function prepend(element, list) {
+  return {
+    value: element,
+    rest: list
+  }
+}
+
+console.log(prepend(57, list));
+
+// function nth(list, position) {
+//   // console.log(`${list.value} ${position}`);
+//   if (position == 0) {
+//     // console.log(list.value);
+//     return list.value;
+//   } else if (list.value == null) {
+//     return undefined;
+//   } else {
+//     return nth(list.rest, position - 1);
+//   }
+// }
+
+// console.log(nth(list, 0));
+// console.log(nth(list, 2));
+// console.log(nth(list, 7));
+// "undefined" WHAT. THE. FUCK????
+// CO. DO. PIZDY????????????????
+// O H I GET IT
+// I AM REALLY SILLY
+// I DIDN'T RETUR THE nth(list.rest, position - 1) IN THE
+// ELSE BLOCK
+
+function nth(list, position) {
+  if (!list) return undefined;
+  else if (position == 0) return list.value;
+  else return nth(list.rest, position - 1);
+}
+
+console.log(nth(list, 0));
+console.log(nth(list, 2));
+console.log(nth(list, 7));
+console.log(nth(list, 6));
